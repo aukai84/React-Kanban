7fecf5330323dbcb2252b16c45ec7775e93a02ae
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-21 23:11:38 -1000

    server up and getting response

---
+++ server/routes/cardsAPI.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express('router');
+const Card = require('../models').Card;
+
+
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -4,14 +4,22 @@ const flash = require('connect-flash');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
+const methodOverride = require('method-override');
+const cardsAPI = require('./routes/cardsAPI');
 
 const db = require('./models');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
+app.use('api/kanban', cardsAPI);
 
-
+app.get('/', (req, res) => {
+    res.json('sever up');
+})
 
 
 module.exports = app;
