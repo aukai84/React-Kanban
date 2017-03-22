e2e21425afd7dbf1221c29d07a665d940b764783
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-22 08:13:02 -1000

    post new card working.. saving in DB

--- server/routes/cardsAPI.js
+++ server/routes/cardsAPI.js
@@ -2,6 +2,24 @@ const express = require('express');
 const router = express('router');
 const Card = require('../models').Card;
 
+router.get('/', (req, res) => {
+    res.json('home page');
+})
+
+router.post('/new', ({body: {title, priority, created_by, assigned_to}}, res) => {
+    console.log(title)
+    Card.create({
+        title,
+        priority,
+        status: "queue",
+        created_by,
+        assigned_to
+    })
+    .then(card => {res.json(card)})
+    //need to send error messages with connect flash
+    .catch(error => {res.json({"error": "error message"})})
+})
+
 
 
 
--- server/server.js
+++ server/server.js
@@ -6,16 +6,17 @@ const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const methodOverride = require('method-override');
 const cardsAPI = require('./routes/cardsAPI');
+console.log(cardsAPI)
 
 const db = require('./models');
 
 const app = express();
-
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 
-app.use('api/kanban', cardsAPI);
+app.use('/api/kanban', cardsAPI);
 
 app.get('/', (req, res) => {
     res.json('sever up');
