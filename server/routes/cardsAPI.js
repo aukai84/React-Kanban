0ff5a96cfde8fae756a7ab549e81a68182675906
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-22 17:24:32 -1000

    added api routes to return queue in progress and done cardrs

--- server/routes/cardsAPI.js
+++ server/routes/cardsAPI.js
@@ -6,8 +6,45 @@ router.get('/', (req, res) => {
     res.json('home page');
 })
 
+router.get('/queue', ({body: {status}}, res) => {
+    Card.findAll({
+        where: {
+            status: "queue"
+        }
+    })
+    .then(cards => {res.json(cards)})
+    .catch(error => {res.json({"error": "error message"})})
+})
+
+router.get('/in-progress', ({body: {status}}, res) => {
+    Card.findAll({
+        where: {
+            status: "in-progress"
+        }
+    })
+    .then(cards => {res.json(cards)})
+    .catch(error => {res.json({"error": "error message"})})
+})
+
+router.get('/done', ({body: {status}}, res) => {
+    Card.findAll({
+        where: {
+            status: "done"
+        }
+    })
+    .then(cards => {res.json(cards)})
+    .catch(error => {res.json({"error": "error message"})})
+})
+
+router.put('/status/:id', ({body: {status}}, res) => {
+    Card.update({
+        status: req.params.id
+    })
+    .then(card => {res.json(card)})
+    .catch(error => {res.json({"error": "error message"})})
+})
+
 router.post('/new', ({body: {title, priority, created_by, assigned_to}}, res) => {
-    console.log(title)
     Card.create({
         title,
         priority,
@@ -19,7 +56,6 @@ router.post('/new', ({body: {title, priority, created_by, assigned_to}}, res) =>
     //need to send error messages with connect flash
     .catch(error => {res.json({"error": "error message"})})
 })
-
 
 
 
