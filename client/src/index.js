114393cc27067b0ba7f6442959c2375434748c47
Aukai Tirrell <mtirrell@hawaii.edu>
2017-03-29 17:30:28 -1000

    need to commit enw changes

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './index.css';
 
-import {createStore} from 'redux';
+import ReduxThunk from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import {createDevTools} from 'redux-devtools';
 import {Provider} from 'react-redux';
 import cards from './containers/reducers'
-let store = createStore(cards);
+let store = createStore(cards, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(ReduxThunk));
 
 ReactDOM.render(
 <Provider store={store}>
